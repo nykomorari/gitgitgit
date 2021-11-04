f835b00e87338c0a1e6ba401f4bb735c6aa6b44c
nykomorari <93434749+nykomorari@users.noreply.github.com>
2021-11-04 15:30:05 -0400

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,25 @@
+package ProjectTask;
+
+public class Project {
+
+	public static void main(String[] args) {
+
+		int num = 26;
+		boolean isPrime = true;
+
+		for (int i = 2; i <= num; i++)
+
+		{
+			if (num % i == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime) {
+
+			System.out.println(num + " is Prime number ");
+		} else {
+			System.out.println("number is not Prime");
+		}
+	}
+}
