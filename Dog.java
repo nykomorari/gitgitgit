7adcaf34d5733b17637497ad530e81a424763f9d
nykomorari <93434749+nykomorari@users.noreply.github.com>
2021-11-06 17:47:50 -0400

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,75 @@
+package com.syntax.class11;
+
+public class Dog {
+
+	
+	String name;
+	String colors;
+	String breed;
+	double heights;
+	int age;
+	char gender;
+	
+	
+	
+	public static void main(String[] args) {
+
+		Dog Husky = new Dog();
+		Husky.name = "Vulfy";
+		Husky.colors = "Grey";
+		Husky.breed = "Siberian Husky";
+		Husky.heights = 20;
+		Husky.age = 9;
+		Husky.gender ='F';
+		
+		System.out.println("My Husky name is "+Husky.name);
+		System.out.println("My Husky color is "+Husky.colors);
+		System.out.println("My Husky breed is "+Husky.breed);
+		System.out.println("My Husky age is "+Husky.age);
+		System.out.println("My Husky gender is "+Husky.gender);
+		
+		System.out.println("*****************************");
+		
+		Dog Bulldog = new Dog();
+		Bulldog.name = "Rex";
+		Bulldog.colors = "Black";
+		Bulldog.breed = "Dog";
+		Bulldog.heights = 12;
+		Bulldog.age = 7;
+		Bulldog.gender = 'M';
+		
+		System.out.println("My Bulldog name is "+Bulldog.name);
+		System.out.println("My Bulldog name is "+Bulldog.colors);
+		System.out.println("My Bulldog name is "+Bulldog.breed);
+		System.out.println("My Bulldog name is "+Bulldog.age);
+		System.out.println("My Bulldog name is "+Bulldog.gender);
+		
+		System.out.println("******************************");
+		
+		Dog Labrador = new Dog();
+		Labrador.name = "Leo";
+		Labrador.colors = "White";
+		Labrador.breed = "Labrador Retriever";
+		Labrador.heights = 15;
+		Labrador.age = 12;
+		Labrador.gender = 'M';
+		
+		System.out.println("My Labrador name is "+Labrador.name);
+		System.out.println("My Labrador name is "+Labrador.colors);
+		System.out.println("My Labrador name is "+Labrador.breed);
+		System.out.println("My Labrador name is "+Labrador.age);
+		System.out.println("My Labrador name is "+Labrador.gender);
+
+
+
+
+	
+
+
+
+
+		
+		
+	}
+
+}
